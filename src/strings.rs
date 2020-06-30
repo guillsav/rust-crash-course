6e60f6ed00a3f71793cd16a3541550e989a6928b
guillsav <guillsav@gmail.com>
2020-06-30 13:01:47 -0400

    Added typles.rs

--- src/strings.rs
+++ src/strings.rs
@@ -13,5 +13,32 @@ pub fn run() {
   // Push a string
   hello.push_str("orld!");
 
-  println!("{}", hello);
+  // Capacity in bytes
+  println!("Capacity: {}", hello.capacity());
+
+  // Check if empty
+  println!("Is Empty: {}", hello.is_empty());
+
+  // Contains
+  println!("Contains 'World': {}", hello.contains("World"));
+
+  // Replace
+  println!("Replace: {}", hello.replace("World", "There"));
+
+  // Loop through string by whitespace
+  for word in hello.split_whitespace() {
+    println!("{}", word);
+  }
+
+  // Create string with capacity
+  let mut s = String::with_capacity(10);
+  s.push('a');
+  s.push('b');
+
+  // Assertion testing
+  assert_eq!(2, s.len());
+  assert_eq!(11, s.capacity());
+
+  println!("{}", s);
+
 }
